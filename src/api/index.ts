c0255a96876dee5a95c8dcad7a3a0cc6495d6658
LucianaH <lhoszowski@fi.uba.ar>
2025-09-21 12:06:16 -0300

    feat(api): add axiosInstance and interceptors

---
+++ src/api/index.ts
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import router from '../router'
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.BASE_URL, // from .env files
+})
+
+axiosInstance.interceptors.request.use((config) => {
+  // Add Bearer token or other headers
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // Handle logout or redirect to login
+      console.error('Not authenticated, please login...')
+      router.push('/login')
+    }
+    if (error.response?.status === 403) {
+      // Handle logout or redirect to login
+      console.error("Unauthorized, you don't have the right credentials")
+      router.push('/')
+    }
+    // Show error message
+    return Promise.reject(error)
+  }
+)
+
+export default axiosInstance
