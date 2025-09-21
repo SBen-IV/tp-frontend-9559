5319d3452eaa1098308bbfb6fb6a21289ef67934
LucianaH <lhoszowski@fi.uba.ar>
2025-09-21 15:07:18 -0300

    fix(registration): change usuarioCrearSchema

--- src/models/users.ts
+++ src/models/users.ts
@@ -3,10 +3,10 @@ import * as z from "zod";
 const roles = ["CLIENTE", "EMPLEADO"] as const;
 
 export const usuarioCrearSchema = z.object({
-  nombre: z.string("Ingrese su nombre").min(2, "Demasiado corto"),
-  apellido: z.string("Ingrese su apellido").min(2, "Demasiado corto"),
+  nombre: z.string("Ingrese su nombre"),
+  apellido: z.string("Ingrese su apellido"),
   email: z.email("Mail inválido"),
-  contraseña: z.string("Ingrese una contraseña").min(2, "Demasiado corta"),
+  contraseña: z.string("Ingrese una contraseña").min(8, "Demasiado corta"),
   rol: z.enum(roles, "Elija su rol"),
 });
 
