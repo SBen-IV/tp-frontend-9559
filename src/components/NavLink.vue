deb478caaa3d4fd37c2c8a6913f4dc04606382fb
LucianaH <lhoszowski@fi.uba.ar>
2025-09-25 10:24:10 -0300

    feat(navigation): add NavLink component

---
+++ src/components/NavLink.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+
+defineProps<{ href: string }>();
+
+</script>
+
+<template>
+    <Button variant='link' class='hover:no-underline'>
+      <RouterLink :to="href" class='text-foreground hover:text-primary'>
+      <slot/>
+      </RouterLink>
+    </Button>
+</template>
--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,12 +1,14 @@
 <script setup lang='ts'>
 import { Button } from '@/components/ui/button'
-import  ModeToggle  from '@/components/ModeToggle.vue'
+import ModeToggle from '@/components/ModeToggle.vue'
 import { useCookies } from "vue3-cookies";
+import NavLink from '@/components/NavLink.vue';
 
 const { cookies } = useCookies();
 </script>
 
 <template>
+
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -17,23 +19,35 @@ const { cookies } = useCookies();
       <a href="/" class="text-2xl font-bold">ITIL</a>
       <ul class="flex space-x-4">
         <Button variant='link' class='hover:no-underline'>
-        <li>
-          <a href="/" class="text-foreground hover:text-primary">Home</a>
-        </li>
+          <li>
+            <a href="/" class="text-foreground hover:text-primary">Home</a>
+          </li>
         </Button>
+
+        <NavLink href="/register">Hola?</NavLink>
+
+        <li>
+          <Button variant='link' class='hover:no-underline'>
+            <RouterLink to="/register">
+              RegisterTesting
+            </RouterLink>
+          </Button>
+        </li>
+
+
 
         <Button variant='link' class='hover:no-underline'>
-        <li>
-          <a href="/login" class="text-foreground hover:text-primary">Ingresar</a>
-        </li>
+          <li>
+            <a href="/login" class="text-foreground hover:text-primary">Ingresar</a>
+          </li>
         </Button>
 
         <li>
-        <Button>
-          <a href="/register" >Registrarse</a>
-        </Button>
+          <Button>
+            <a href="/register">Registrarse</a>
+          </Button>
         </li>
-      <ModeToggle/>
+        <ModeToggle />
       </ul>
     </nav>
   </header>
