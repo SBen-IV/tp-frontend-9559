bebb51aa96a5e33bf07b78bea52fa9f0a8d661cc
Luciana Hoszowski <lhoszowski@fi.uba.ar>
2025-09-24 23:07:53 -0300

    refactor(User): change schema

--- src/models/users.ts
+++ src/models/users.ts
@@ -3,11 +3,11 @@ import * as z from "zod";
 const roles = ["CLIENTE", "EMPLEADO"] as const;
 
 export const userCreateSchema = z.object({
-  nombre: z.string().min(1, "Ingrese su nombre"),
-  apellido: z.string().min(1, "Ingrese su apellido"),
-  email: z.string().email("Mail inválido"),
-  contraseña: z.string("Ingrese una contraseña").min(8, "Demasiado corta"),
-  rol: z.enum(roles, "Elija su rol"),
+  nombre: z.string({required_error: 'Ingrese su nombre'}).min(1, "Ingrese su nombre"),
+  apellido: z.string({required_error: 'Ingrese su apellido'}).min(1, "Ingrese su apellido"),
+  email: z.string({required_error: 'Ingrese su email'}).email("Mail inválido"),
+  contraseña: z.string({required_error: 'Ingrese una contraseña'}).min(8, "Demasiado corta"),
+  rol: z.enum(roles),
 });
 
 export type UserCreate = z.infer<typeof userCreateSchema>;
