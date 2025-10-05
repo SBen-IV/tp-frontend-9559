2fbc7e634d529118d56cf0bf1ce88ad66efca018
Luciana Hoszowski <lhoszowski@fi.uba.ar>
2025-10-05 14:47:27 -0300

    feat(api): get changes

--- src/api/changes.ts
+++ src/api/changes.ts
@@ -1,8 +1,18 @@
-import type { ChangeCreate } from "@/models/changes";
+import type { Change, ChangeCreate } from "@/models/changes";
 import axiosInstance from "./index";
 
 const BASE_URL: string = "/api/v1/changes";
 
 export async function createChange(change: ChangeCreate) {
   return await axiosInstance.post(BASE_URL, change);
+}
+
+export async function getChangeByID(changeID: string) {
+  return await axiosInstance.get(`${BASE_URL}/${changeID}`);
+}
+
+export async function getAllChanges() : Promise<Change[]> {
+  const response =  await axiosInstance.get(`${BASE_URL}`);
+  
+  return response.data
 }
