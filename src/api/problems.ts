a5b6cc9bc4a99e7f2167b9f59e36ddfb9cf9606a
SBen-IV <72094271+SBen-IV@users.noreply.github.com>
2025-10-08 19:53:36 -0300

    feat(problems): view list of problems and each problem

--- src/api/problems.ts
+++ src/api/problems.ts
@@ -1,8 +1,14 @@
 import axiosInstance from "./index";
-import type { ProblemCreate } from "@/models/problems";
+import type { Problem, ProblemCreate } from "@/models/problems";
 
 const BASE_URL: string = "/api/v1/problems";
 
 export async function createProblem(problem: ProblemCreate) {
   return await axiosInstance.post(BASE_URL, problem);
+}
+
+export async function getAllProblems(): Promise<Problem[]> {
+  const response = await axiosInstance.get(BASE_URL);
+
+  return response.data;
 }
