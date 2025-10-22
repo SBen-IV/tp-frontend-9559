738630c0d45f90e1318030207d349fcd8becb1c5
SBen-IV <72094271+SBen-IV@users.noreply.github.com>
2025-10-22 20:53:00 -0300

    feat(problem): tweaked incident option

--- src/components/IncidentOption.vue
+++ src/components/IncidentOption.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { Incident } from "@/models/incidents";
-import { CodeXml, Wrench, FileText } from "lucide-vue-next";
+import { CodeXml, Wrench, FileText, ShieldAlert, Plug } from "lucide-vue-next";
 import { computed } from "vue";
 
 const props = defineProps<{ incident: Incident }>();
@@ -8,7 +8,8 @@ const props = defineProps<{ incident: Incident }>();
 const categoryIcons: Record<string, any> = {
   SOFTWARE: CodeXml,
   HARDWARE: Wrench,
-  DOCUMENTACION: FileText,
+  SEGURIDAD: ShieldAlert,
+  SOLICITUD_DE_SERVICIO: Plug,
 };
 
 const itemIcon = computed(() => {
@@ -20,20 +21,18 @@ const itemIcon = computed(() => {
   <div
     class="flex items-center gap-1.5 w-full min-w-[50px] max-w-[180px] px-1.5 py-1"
   >
-    <!--
     <component
       :is="itemIcon"
       class="w-3 h-3 flex-shrink-0 text-muted-foreground"
     />
--->
     <div class="flex-1 min-w-0">
       <div class="flex items-center justify-between gap-1">
         <span class="text-xs font-medium wrap">{{ incident.titulo }}</span>
         <span
-          v-if="incident.categoria"
+          v-if="incident.estado"
           class="text-[10px] text-muted-foreground flex-shrink-0"
         >
-          {{ incident.categoria }}
+          {{ incident.estado }}
         </span>
       </div>
       <div class="text-[10px] text-muted-foreground wrap">
