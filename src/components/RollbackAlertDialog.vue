f07f1b525fcb8c9251fb04d821f92b483478d958
LucianaH <lhoszowski@fi.uba.ar>
2025-10-26 00:53:50 -0300

    feat: add RollbackAlertDialog component

---
+++ src/components/RollbackAlertDialog.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+import { History } from "lucide-vue-next";
+
+defineProps<{ handleRollback: () => void }>();
+</script>
+
+<template>
+  <AlertDialog>
+    <AlertDialogTrigger as-child>
+      <Button variant="outline" size="icon" aria-label="Restaurar">
+        <History/>
+      </Button>
+    </AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Restaurar versión</AlertDialogTitle>
+        <AlertDialogDescription>
+          ¿Está seguro que quiere continuar?
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancelar</AlertDialogCancel>
+        <AlertDialogAction @click="handleRollback"
+          >Restaurar</AlertDialogAction
+        >
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
