6fa22984a75a03316c4d9b3c8cae6111a5916b64
LucianaH <lhoszowski@fi.uba.ar>
2025-10-26 00:41:29 -0300

    feat(changeHistory): show change versions

--- src/views/ChangeHistory.vue
+++ src/views/ChangeHistory.vue
@@ -1,57 +1,63 @@
 <script setup lang="ts">
-import { getChangeAuditsByID, getChangeByID } from '@/api/changes';
-import type { Change, ChangeAudit, ChangeVersion } from '@/models/changes';
-import { computed, onMounted, ref, shallowRef, watch } from 'vue'
-import { useRoute } from 'vue-router'
-import { getPrioridadColor } from '@/lib/utils';
-import Badge from '@/components/ui/badge/Badge.vue';
-import Separator from '@/components/ui/separator/Separator.vue';
-import Card from '@/components/ui/card/Card.vue';
-import { prettyDate } from '@/lib/utils';
-import ItemOption from '@/components/ItemOption.vue';
-import { getConfigItemById } from '@/api/config_items';
+import { getChangeAuditsByID, getChangeByID } from "@/api/changes";
+import type { Change, ChangeAudit, ChangeVersion } from "@/models/changes";
+import { onMounted, ref, shallowRef, watch } from "vue";
+import { useRoute } from "vue-router";
+import { getPrioridadColor } from "@/lib/utils";
+import Badge from "@/components/ui/badge/Badge.vue";
+import Separator from "@/components/ui/separator/Separator.vue";
+import Card from "@/components/ui/card/Card.vue";
+import { prettyDate } from "@/lib/utils";
+import ItemOption from "@/components/ItemOption.vue";
+import { getConfigItemById } from "@/api/config_items";
+import CardHeader from "@/components/ui/card/CardHeader.vue";
+import CardContent from "@/components/ui/card/CardContent.vue";
 
-const route = useRoute()
+const route = useRoute();
 
 const change = ref<Change>();
 const changeAudits = shallowRef<ChangeAudit[]>([]);
-const changeVersions = ref<ChangeVersion[]>([])
+const changeVersions = ref<ChangeVersion[]>([]);
 
 watch(
   () => changeAudits.value,
   async (audits) => {
-    if (!audits || audits.length === 0) return
+    if (!audits || audits.length === 0) return;
 
     const versions = await Promise.all(
       audits.map(async (audit) => {
         const configItems = await Promise.all(
           audit.estado_nuevo.id_config_items.map((id) => getConfigItemById(id))
-        )
+        );
 
-        return {
+        const { estado_nuevo, ...auditWithoutEstadoNuevo } = audit;
+
+        const changeVersion: ChangeVersion = {
           ...audit.estado_nuevo,
+          ...auditWithoutEstadoNuevo,
           config_items: configItems,
-          id_auditoria: audit.id,
-        } as ChangeVersion
-      })
-    )
+        };
 
-    changeVersions.value = versions
+        return changeVersion;
+      })
+    );
+
+    changeVersions.value = versions;
   }
-)
+);
 
 const fetchChange = async (changeID: string) => {
-  change.value = await getChangeByID(changeID)
-  console.log('Change data:', change.value); 
-}
+  change.value = await getChangeByID(changeID);
+  console.log("Change data:", change.value);
+};
 
 const fetchChangeHistory = async (changeID: string) => {
-  changeAudits.value = await getChangeAuditsByID(changeID)
-}
+  changeAudits.value = await getChangeAuditsByID(changeID);
+};
 
 onMounted(() => {
   fetchChange(route.params.id as string);
-  fetchChangeHistory(route.params.id as string)
+  fetchChangeHistory(route.params.id as string);
 });
 </script>
 
@@ -94,14 +100,18 @@ onMounted(() => {
       <Separator class="my-4" />
 
       <div>
-        <p class="font-medium text-muted-foreground mb-1">Ítems de configuración</p>
-        <div class="flex">
-          <ItemOption
-            v-for="item in change.config_items"
-            :key="item.id"
-            :item="item"
-            class="hover:bg-accent rounded-md mb-2 pt-4"
-          />
+        <p class="font-medium text-muted-foreground mb-1">
+          Ítems de configuración
+        </p>
+        <div class="max-h-[120px] overflow-y-auto">
+          <div class="grid grid-cols-1 sm:grid-cols-5 gap-2">
+            <ItemOption
+              v-for="item in change.config_items"
+              :key="item.id"
+              :item="item"
+              class="hover:bg-accent rounded-md p-2"
+            />
+          </div>
         </div>
       </div>
     </Card>
@@ -109,18 +119,69 @@ onMounted(() => {
     <Separator class="my-4" />
 
     <div>
-      <h3 class="text-2xl font-bold mb-4">Historial de Versiones</h3>
-        <div v-if="changeAudits.length > 0" class="space-y-4">
-          <div v-for="version in changeVersions" :key="version.id_auditoria">
-              <Card >
-                  {{ version.id_auditoria }}
-              </Card>
-          </div>
-        </div>
-        <div v-else>
-          No se encontraron versiones anteriores para este cambio.
-        </div>
-    </div>
+      <h3 class="text-xl font-bold mb-4">Historial de Versiones</h3>
+      <div v-if="changeAudits.length > 0" class="space-y-4">
+        <Card v-for="version in changeVersions" :key="version.id" class="p-6">
+          <CardHeader class="grid grid-cols-3">
+            <div class="flex space-x-3">
+              <span class="text-xl font-medium text-left">
+                {{ prettyDate(version.fecha_actualizacion) }}
+              </span>
+              <Badge
+                variant="outline"
+                class="font-medium text-muted-foreground content-right"
+              >
+                {{ version.operacion }}
+              </Badge>
+            </div>
 
+            <span class="text-xl font-medium text-center">
+              {{ version.titulo }}
+            </span>
+          </CardHeader>
+
+          <CardContent class="grid grid-cols-3 gap-6 text-sm">
+            <div class="col-span-2 space-y-6">
+              <div class="grid grid-cols-2 gap-6">
+                <div>
+                  <p class="text-muted-foreground mb-1 font-medium">Estado</p>
+                  <Badge variant="secondary">{{ version.estado }}</Badge>
+                </div>
+                <div>
+                  <p class="text-muted-foreground mb-1 font-medium">
+                    Prioridad
+                  </p>
+                  <Badge :class="getPrioridadColor(version.prioridad)">
+                    {{ version.prioridad }}
+                  </Badge>
+                </div>
+              </div>
+
+              <div class="max-h-[20dvh] overflow-y-auto">
+                <p class="text-muted-foreground mb-1 font-medium">
+                  Descripción
+                </p>
+                <p class="text-sm leading-relaxed">{{ version.descripcion }}</p>
+              </div>
+            </div>
+
+            <div class="row-span-2">
+              <p class="text-muted-foreground mb-2 font-medium">
+                Ítems de configuración
+              </p>
+              <div class="flex flex-col gap-2 max-h-60 overflow-y-auto pr-2">
+                <ItemOption
+                  v-for="item in version.config_items"
+                  :key="item.id"
+                  :item="item"
+                  class="hover:bg-accent rounded-md max-w-full"
+                />
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+      <div v-else>No se encontraron versiones anteriores para este cambio.</div>
+    </div>
   </div>
 </template>
