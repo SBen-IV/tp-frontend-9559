233006a554c4e74095119d046f4c0f670b77c1e6
LucianaH <lhoszowski@fi.uba.ar>
2025-10-31 19:23:40 -0300

    feat(config_items): add ConfigItemHistory and rollback functionality

--- src/views/ConfigurationItemPage.vue
+++ src/views/ConfigurationItemPage.vue
@@ -1,17 +1,32 @@
 <script setup lang="ts">
-import { getConfigItemAuditsByID, getConfigItemById } from "@/api/config_items";
-import type { ConfigItem, ConfigItemAudit, ConfigItemVersion } from "@/models/config_items";
+import { getConfigItemAuditsByID, getConfigItemById, rollbackConfigItem } from "@/api/config_items";
+import type {
+  ConfigItem,
+  ConfigItemAudit,
+  ConfigItemVersion,
+} from "@/models/config_items";
 import { onMounted, ref, shallowRef, watch } from "vue";
 import { useRoute } from "vue-router";
 import Separator from "@/components/ui/separator/Separator.vue";
 import { toast } from "vue-sonner";
 import ConfigItemInfo from "@/components/ConfigItemInfo.vue";
+import ConfigItemHistory from "@/components/ConfigItemHistory.vue";
 
 const route = useRoute();
 
 const configItem = ref<ConfigItem>();
 const configItemAudits = shallowRef<ConfigItemAudit[]>([]);
 const configItemVersions = ref<ConfigItemVersion[]>([]);
+
+const handleRollback = async (versionID: string) => {
+  try {
+    await rollbackConfigItem(route.params.id as string, versionID);
+    toast.success("Se restauró el ítem correctamente");
+    fetchConfigItem();
+  } catch (err: any) {
+    toast.error(err.message);
+  }
+};
 
 watch(
   () => configItemAudits.value,
@@ -24,8 +39,8 @@ watch(
 
         // We remove nested structure from the audit and we add the config_items
         const configItemVersion: ConfigItemVersion = {
-          ...audit.estado_nuevo,  // the configItem version
-          ...auditWithoutEstadoNuevo, // fecha_actualizacion, (audit) ID, ... 
+          ...audit.estado_nuevo, // the configItem version
+          ...auditWithoutEstadoNuevo, // fecha_actualizacion, (audit) ID, ...
         };
 
         return configItemVersion;
@@ -41,7 +56,7 @@ const fetchConfigItem = async () => {
     const configItemID = route.params.id as string;
     configItem.value = await getConfigItemById(configItemID);
   } catch (error: any) {
-    toast.error(error.message || "Error al cargar el ítem")
+    toast.error(error.message || "Error al cargar el ítem");
   }
 };
 
@@ -50,7 +65,7 @@ const fetchConfigItemHistory = async () => {
     const configItemID = route.params.id as string;
     configItemAudits.value = await getConfigItemAuditsByID(configItemID);
   } catch (error: any) {
-    toast.error(error.message || "Error al cargar el historial")
+    toast.error(error.message || "Error al cargar el historial");
   }
 };
 
@@ -63,5 +78,18 @@ onMounted(() => {
 <template>
   <div v-if="configItem" class="space-y-6">
     <ConfigItemInfo :config-item="configItem" />
+
+    <Separator class="my-4" />
+
+    <h3 class="text-xl font-bold mb-4">Historial de Versiones</h3>
+
+    <div v-if="configItemAudits.length > 0" class="space-y-4 mb-5">
+      <ConfigItemHistory
+        :config-item-versions="configItemVersions"
+        :handle-rollback="handleRollback"
+      />
+    </div>
+
+    <div v-else>No se encontraron versiones anteriores para este cambio.</div>
   </div>
 </template>
