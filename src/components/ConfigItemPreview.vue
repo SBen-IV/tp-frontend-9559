c38dfba47cbbe7de81c1e6e90dd9bcbb46ce1491
LucianaH <lhoszowski@fi.uba.ar>
2025-10-31 18:54:46 -0300

    feat(config_items): link to ConfigItemPage in item card

--- src/components/ConfigItemPreview.vue
+++ src/components/ConfigItemPreview.vue
@@ -16,6 +16,7 @@ import {
   Eye,
   Pencil,
   ArrowLeft,
+  History,
 } from "lucide-vue-next";
 import { computed, ref } from "vue";
 import {
@@ -135,7 +136,18 @@ const handleDelete = async () => {
               </p>
             </div>
             <DialogFooter>
-              <div class="flex gap-2 pb-4 px-4">
+              <div class="w-full flex justify-between items-center pb-4 px-4 pt-4">
+                <div class="flex">
+                  <Button variant="secondary" class="relative p-0">
+                    <RouterLink 
+                      :to="`/config-items/${item.id}`" 
+                      class="flex items-center gap-2 w-full h-full px-4 py-2"
+                    >
+                      <History class="w-2 h-4"/>
+                      Historial
+                    </RouterLink>
+                  </Button>
+                </div>
                 <!-- TODO: These buttons should have actions associated -->
                 <Button @click="editView = true">
                   <Pencil class="w-2 h-4" />Edit</Button
---
+++ src/views/ConfigurationItemPage.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { getConfigItemAuditsByID, getConfigItemById } from "@/api/config_items";
+import type { ConfigItem, ConfigItemAudit, ConfigItemVersion } from "@/models/config_items";
+import { onMounted, ref, shallowRef, watch } from "vue";
+import { useRoute } from "vue-router";
+import Separator from "@/components/ui/separator/Separator.vue";
+import { toast } from "vue-sonner";
+
+const route = useRoute();
+
+const configItem = ref<ConfigItem>();
+const configItemAudits = shallowRef<ConfigItemAudit[]>([]);
+const configItemVersions = ref<ConfigItemVersion[]>([]);
+
+watch(
+  () => configItemAudits.value,
+  async (audits) => {
+    if (!audits || audits.length === 0) return;
+
+    const versions = await Promise.all(
+      audits.map(async (audit) => {
+        const { estado_nuevo, ...auditWithoutEstadoNuevo } = audit;
+
+        // We remove nested structure from the audit and we add the config_items
+        const configItemVersion: ConfigItemVersion = {
+          ...audit.estado_nuevo,  // the configItem version
+          ...auditWithoutEstadoNuevo, // fecha_actualizacion, (audit) ID, ... 
+        };
+
+        return configItemVersion;
+      })
+    );
+
+    configItemVersions.value = versions;
+  }
+);
+
+const fetchConfigItem = async () => {
+  try {
+    const configItemID = route.params.id as string;
+    configItem.value = await getConfigItemById(configItemID);
+  } catch (error: any) {
+    toast.error(error.message || "Error al cargar el ítem")
+  }
+};
+
+const fetchConfigItemHistory = async () => {
+  try {
+    const configItemID = route.params.id as string;
+    configItemAudits.value = await getConfigItemAuditsByID(configItemID);
+  } catch (error: any) {
+    toast.error(error.message || "Error al cargar el historial")
+  }
+};
+
+onMounted(() => {
+  fetchConfigItem();
+  fetchConfigItemHistory();
+});
+</script>
+
+<template>
+  <div v-if="configItem" class="space-y-6">
+    Config Item {{ configItem.id }}
+  </div>
+</template>
