463b9c4d3277bb1c35daeb41f47acbdb6e72335d
SBen-IV <72094271+SBen-IV@users.noreply.github.com>
2025-12-06 23:42:16 -0300

    feat(change): show incidents and problems in actual version

--- src/components/ChangeInfo.vue
+++ src/components/ChangeInfo.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import type { Change } from '@/models/changes';
-import { getImpactoColor, getPrioridadColor } from '@/lib/utils';
-import { prettyDate } from '@/lib/utils';
-import Card from './ui/card/Card.vue';
-import Badge from './ui/badge/Badge.vue';
-import Separator from './ui/separator/Separator.vue';
-import ItemOption from './ItemOption.vue';
-import ResponsableInfo from './ResponsableInfo.vue';
+import type { Change } from "@/models/changes";
+import { getImpactoColor, getPrioridadColor } from "@/lib/utils";
+import { prettyDate } from "@/lib/utils";
+import Card from "./ui/card/Card.vue";
+import Badge from "./ui/badge/Badge.vue";
+import Separator from "./ui/separator/Separator.vue";
+import ItemOption from "./ItemOption.vue";
+import ResponsableInfo from "./ResponsableInfo.vue";
+import IncidentOption from "./IncidentOption.vue";
+import ProblemOption from "./ProblemOption.vue";
 
 defineProps<{ change: Change }>();
 </script>
@@ -62,21 +64,60 @@ defineProps<{ change: Change }>();
 
     <Separator class="my-4" />
 
-    <div>
-      <p class="font-medium text-muted-foreground mb-1">
-        Ítems de configuración
-      </p>
-      <div class="max-h-[120px] overflow-y-auto">
-        <div v-if="change.config_items.length > 0" class="grid grid-cols-1 sm:grid-cols-5 gap-2">
-          <ItemOption
-            v-for="item in change.config_items"
-            :key="item.id"
-            :item="item"
-            class="hover:bg-accent rounded-md p-2"
-          />
+    <div class="grid grid-cols-3">
+      <div>
+        <p class="font-medium text-muted-foreground mb-1">
+          Ítems de configuración
+        </p>
+        <div class="max-h-[120px] overflow-y-auto">
+          <div
+            v-if="change.config_items.length > 0"
+            class="flex flex-col gap-2 max-h-60 pr-2"
+          >
+            <ItemOption
+              v-for="item in change.config_items"
+              :key="item.id"
+              :item="item"
+              class="hover:bg-accent rounded-md max-w-full"
+            />
+          </div>
+          <div v-else class="text-sm">Sin ítem relacionados.</div>
         </div>
-        <div v-else class="text-sm">
-          Sin ítem relacionados.
+      </div>
+
+      <div>
+        <p class="font-medium text-muted-foreground mb-1">Problemas</p>
+        <div class="max-h-[120px] overflow-y-auto">
+          <div
+            v-if="change.problemas.length > 0"
+            class="flex flex-col gap-2 max-h-60 pr-2"
+          >
+            <ProblemOption
+              v-for="problem in change.problemas"
+              :key="problem.id"
+              :problem="problem"
+              class="hover:bg-accent rounded-md max-w-full"
+            />
+          </div>
+          <div v-else class="text-sm">Sin problemas relacionados.</div>
+        </div>
+      </div>
+
+      <div>
+        <p class="font-medium text-muted-foreground mb-1">Incidentes</p>
+        <div class="max-h-[120px] overflow-y-auto">
+          <div
+            v-if="change.incidentes.length > 0"
+            class="flex flex-col gap-2 max-h-60 pr-2"
+          >
+            <IncidentOption
+              v-for="incident in change.incidentes"
+              :key="incident.id"
+              :incident="incident"
+              class="hover:bg-accent rounded-md max-w-full"
+            />
+          </div>
+          <div v-else class="text-sm">Sin incidentes relacionados.</div>
         </div>
       </div>
     </div>
